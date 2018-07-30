6917d3f540fa01002083a3170b8f095759e3d6d0
Takatoshi Ono <takatoshi.ono@gmail.com>
2018-07-31 06:13:35 +0900

    ConversationIdは文字列にする make ConversationId string
    to make it difficult to guess and easy to generate.

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -14,17 +14,17 @@ func NewMessageServer() *server {
 }
 
 func (s *server) CreateConversation(context.Context, *pb.CreateConversationRequest) (*pb.Conversation, error) {
-	conversation := &pb.Conversation{Id: 1, CreatedAt: time.Now().Unix()}
+	conversation := &pb.Conversation{Id: "abc123", CreatedAt: time.Now().Unix()}
 	return conversation, nil
 }
 
 func (s *server) CreateMessage(context.Context, *pb.CreateMessageRequest) (*pb.Message, error) {
-	message := &pb.Message{ConversationId: 1, Body: "Hello", CreatedAt: time.Now().Unix()}
+	message := &pb.Message{ConversationId: "abc123", Body: "Hello", CreatedAt: time.Now().Unix()}
 	return message, nil
 }
 
 func (s *server) GetMessages(context.Context, *pb.GetMessagesRequest) (*pb.Messages, error) {
-	message := &pb.Message{ConversationId: 1, Body: "Hello", CreatedAt: time.Now().Unix()}
+	message := &pb.Message{ConversationId: "abc123", Body: "Hello", CreatedAt: time.Now().Unix()}
 	messages := &pb.Messages{Messages: []*pb.Message{message}}
 	return messages, nil
 }
