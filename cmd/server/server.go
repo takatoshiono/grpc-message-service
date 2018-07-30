d3598a37a61aa24f4aa29b30d57afc2ed9bd4224
Takatoshi Ono <takatoshi.ono@gmail.com>
2018-07-31 06:35:27 +0900

    return received message

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -13,17 +13,17 @@ func NewMessageServer() *server {
 	return &server{}
 }
 
-func (s *server) CreateConversation(context.Context, *pb.CreateConversationRequest) (*pb.Conversation, error) {
+func (s *server) CreateConversation(ctx context.Context, in *pb.CreateConversationRequest) (*pb.Conversation, error) {
 	conversation := &pb.Conversation{Id: "abc123", CreatedAt: time.Now().Unix()}
 	return conversation, nil
 }
 
-func (s *server) CreateMessage(context.Context, *pb.CreateMessageRequest) (*pb.Message, error) {
-	message := &pb.Message{ConversationId: "abc123", Sender: "alice", Body: "Hello", CreatedAt: time.Now().Unix()}
+func (s *server) CreateMessage(ctx context.Context, in *pb.CreateMessageRequest) (*pb.Message, error) {
+	message := &pb.Message{ConversationId: in.ConversationId, Sender: in.Sender, Body: in.Body, CreatedAt: time.Now().Unix()}
 	return message, nil
 }
 
-func (s *server) GetMessages(context.Context, *pb.GetMessagesRequest) (*pb.Messages, error) {
+func (s *server) GetMessages(ctx context.Context, in *pb.GetMessagesRequest) (*pb.Messages, error) {
 	message := &pb.Message{ConversationId: "abc123", Sender: "alice", Body: "Hello", CreatedAt: time.Now().Unix()}
 	messages := &pb.Messages{Messages: []*pb.Message{message}}
 	return messages, nil
