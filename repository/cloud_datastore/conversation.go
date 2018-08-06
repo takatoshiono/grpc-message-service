622d4c43104800d0199d901aebaa7ed2f90cf742
Takatoshi Ono <takatoshi.ono@gmail.com>
2018-08-06 13:38:02 +0900

    save conversation to the Cloud Datastore

--- repository/cloud_datastore/conversation.go
+++ repository/cloud_datastore/conversation.go
@@ -1,15 +1,36 @@
 package cloud_datastore
 
 import (
+	"context"
+	"log"
+
+	"cloud.google.com/go/datastore"
+
 	"github.com/takatoshiono/grpc-message-service/entity"
 )
 
-type ConversationRespository struct{}
+const projectID = "grpc-message-service"
+
+type ConversationRespository struct {
+	client *datastore.Client
+}
 
 func NewConversationRepository() *ConversationRespository {
-	return &ConversationRespository{}
+	ctx := context.Background()
+	client, err := datastore.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	return &ConversationRespository{client: client}
 }
 
 func (r *ConversationRespository) Save(c *entity.Conversation) (*entity.Conversation, error) {
+	ctx := context.Background()
+	k := datastore.NameKey("Conversation", c.Id, nil)
+	_, err := r.client.Put(ctx, k, c)
+	if err != nil {
+		log.Fatalf("Failed to save conversation: %v", err)
+	}
 	return c, nil
 }
